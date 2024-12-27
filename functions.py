164fda3379fe301cf268f2748eb941cb8aa9e175
donehis <doegbenyonw@gmail.com>
2024-12-27 23:55:03 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+def get_todos(filepath="todos.txt"):
+    """ Read the text file and return a list of to-do """
+    with open(filepath, "r") as file_r:
+        todos_read = file_r.readlines()
+    return todos_read
+
+def write_todos(todos_write, filepath_w="todos.txt"):
+    """ Write the to-do list to the text file"""
+    with open(filepath_w, "w") as file_w:
+        file_w.writelines(todos_write)
+
+
+if __name__ == "__main__":
+    print(get_todos())
