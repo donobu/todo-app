1d6764fb77ad0aa37fa966aec186e479e5c16b17
donehis <doegbenyonw@gmail.com>
2025-01-09 23:08:19 +0000

    changed theme, add error handling and clock

--- todogui.py
+++ todogui.py
@@ -1,6 +1,10 @@
 import functions
 import FreeSimpleGUI as fsg
+import time
 
+fsg.theme('DarkBlue5')
+
+clock_label = fsg.Text("",key="clock", text_color="yellow")
 label1 = fsg.Text("Type in a to-do ")
 input1 = fsg.Input(tooltip="Enter a todo", key="to-do")
 button1 = fsg.Button("Add")
@@ -10,18 +14,15 @@ edit_button = fsg.Button("Edit")
 complete_button = fsg.Button("Complete")
 exit_button = fsg.Button("Exit")
 
-layout = [[label1], [input1, button1],
+layout = [[clock_label], [label1], [input1, button1],
           [list_box, edit_button],
           [complete_button, exit_button]]
 
 window = fsg.Window("My To-do App", layout=layout, font=("Helvetica", 14))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    """print(3, values['to-dos'][0])
-    print(4, values['to-do'])"""
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y  %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -32,24 +33,30 @@ while True:
             # add button
             window['to-dos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['to-dos'][0]
-            new_todo = values['to-do']
+            try:
+                todo_to_edit = values['to-dos'][0]
+                new_todo = values['to-do']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + "\n"
-            functions.write_todos(todos)
-            # update the listbox instance with a key named to-dos when the user click on edit with
-            # anything on the input box
-            window['to-dos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + "\n"
+                functions.write_todos(todos)
+                # update the listbox instance with a key named to-dos when the user click on edit with
+                # anything on the input box
+                window['to-dos'].update(values=todos)
+            except (IndexError, ValueError):
+                fsg.popup("Please, select an item first!", font=("Helvetica", 14))
         case "Complete":
-            todo_to_complete = values["to-dos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            # update both list box and input field
-            window['to-dos'].update(values=todos)
-            window['to-do'].update(value="")
+            try:
+                todo_to_complete = values["to-dos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                # update both list box and input field
+                window['to-dos'].update(values=todos)
+                window['to-do'].update(value="")
+            except (IndexError, ValueError):
+                fsg.popup("Please, select an item first!", font=("Helvetica", 14))
         case "Exit":
             break
         case "to-dos":
@@ -57,7 +64,5 @@ while True:
             window['to-do'].update(value=values['to-dos'][0])
         case fsg.WIN_CLOSED:
             break
-
-
 
 window.close()
