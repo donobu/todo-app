59e0057e0ac0397ce262ab77c2d7c81f200d2ae2
donehis <doegbenyonw@gmail.com>
2025-01-09 22:15:56 +0000

    implement the complete and exit button

--- todogui.py
+++ todogui.py
@@ -7,9 +7,12 @@ button1 = fsg.Button("Add")
 list_box = fsg.Listbox(values=functions.get_todos(), key="to-dos",
                        enable_events=True, size=(44, 10))
 edit_button = fsg.Button("Edit")
+complete_button = fsg.Button("Complete")
+exit_button = fsg.Button("Exit")
 
 layout = [[label1], [input1, button1],
-          [list_box, edit_button]]
+          [list_box, edit_button],
+          [complete_button, exit_button]]
 
 window = fsg.Window("My To-do App", layout=layout, font=("Helvetica", 14))
 
@@ -39,6 +42,16 @@ while True:
             # update the listbox instance with a key named to-dos when the user click on edit with
             # anything on the input box
             window['to-dos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["to-dos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            # update both list box and input field
+            window['to-dos'].update(values=todos)
+            window['to-do'].update(value="")
+        case "Exit":
+            break
         case "to-dos":
             # update the input box instance with a key named to-do when the listbox is clicked
             window['to-do'].update(value=values['to-dos'][0])
